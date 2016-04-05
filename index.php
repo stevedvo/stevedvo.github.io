bc0acf2cf49124dc5834bcdfdbc125dc006c9a95
stevedvo <steve.deveaux@blueyonder.co.uk>
2016-04-05 21:48:05 +0100

    Added Twitter widget, plus a few minor style tweaks/text changes.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 	<head>
 		<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 		
 	</head>
 
