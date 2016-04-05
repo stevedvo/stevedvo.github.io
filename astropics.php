a905949c52f6c36bbf68d8e4749b35687de5692f
stevedvo <steve.deveaux@blueyonder.co.uk>
2016-04-05 13:52:12 +0100

    Added PHP versions of pages, and using 'includes' function

---
+++ astropics.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+		<link rel="stylesheet" type="text/css" href="main.css"/>
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+	</head>
+
+	<body>
+		<div id="wrapper" class="col-lg-12 col-md-12 col-sm-12">
+
+			<?php
+				$page_title = "Astro Pics";
+				include ("includes/header.html");
+				include ("includes/astropics.html");
+
+			?>
+
+		</div>
+		
+	<body>
+
+<html>
