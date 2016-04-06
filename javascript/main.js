9f2b276ae7958f92e3cab1349f12f39858f9d5f6
stevedvo <steve.deveaux@blueyonder.co.uk>
2016-04-06 21:45:41 +0100

    [refs #3] Adding background image and sorting out automatic resizing. More work required to polish how this works on different screen sizes, and shift to the left (remove padding/margin).

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,19 @@
+function resizeBackground ()
+{
+	var xWidth = $(window).width();
+	var yHeight = $(window).height();
+
+	if (yHeight > xWidth)
+	{
+		$("#background").height(yHeight);
+	}
+	else
+	{
+		$("#background").width(xWidth);			
+	}
+
+}
+
 function init ()
 {
 	$(".navitem").mouseover(function()
@@ -17,6 +33,8 @@ function init ()
 		$(this).css('color', 'black');
 	});
 
+	resizeBackground();
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
+window.addEventListener("resize", resizeBackground, false);
