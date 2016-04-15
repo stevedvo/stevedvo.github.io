259ef9b9dbf4daae34231c6bffa29a0d85503acd
stevedvo <steve.deveaux@blueyonder.co.uk>
2016-04-15 14:42:22 +0100

    [refs 28] Added favicon.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<!-- additional options to content to prevent zooming on mobile devices, maximum-scale=1, user-scalable=no"-->
 		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 		<script type="text/javascript" src="javascript/main.js"></script>
+		<link rel="shortcut icon" href="/images/3.ico"/>
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
 		<script type="text/javascript" src="jQuery/jquery-1.12.3.min.js"></script>
 		<script type="text/javascript" src="jQuery/jquery-ui-1.11.4/jquery-ui.min.js"></script>
