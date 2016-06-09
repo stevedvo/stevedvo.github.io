d1180c4b614bf6f0ebb2b3e66e5d0ceebbdf0bc2
stevedvo <steve.deveaux@blueyonder.co.uk>
2016-06-09 16:14:07 +0100

    [refs #49] Converted the Sepsis page to use the jQuery Vertical Tabs. Purged the redundant JavaScript & CSS that related to previous navigation structure. Tidied up & condensed a couple of other bits of CSS.

--- javascript/main.js
+++ javascript/main.js
@@ -12,8 +12,6 @@ function init ()
 	$("#burger").css('color', '#337ab7');
 	$("#filling").hide();
 	
-	$("#filling_2y").hide();
-
 	$(".navitem").mouseover(function()
 	{
 		$(this).css('background', 'yellow');
@@ -24,17 +22,6 @@ function init ()
 	$(".top").mouseout(function()
 	{
 		$(this).animate({backgroundColor: jQuery.Color({alpha: 0}), color: "#337ab7"}, 250);
-	});
-
-	$(".left").mouseover(function()
-	{
-		$(this).css('font-weight', 'bold');
-	});
-
-	$(".left").mouseout(function()
-	{
-		$(this).css('font-weight', 'normal');
-		$(this).animate({backgroundColor: jQuery.Color({alpha: 0}), color: "#white"}, 250);
 	});
 
 	$(".navitem").click(function()
@@ -52,87 +39,6 @@ function init ()
 	$("#filling").click(function()
 	{
 		$("#filling").slideToggle("slow");
-	});
-
-	$("#burger_2y").click(function()
-	{
-		$("#filling_2y").slideToggle("slow");
-	});
-
-	$("#filling_2y").click(function()
-	{
-		$("#filling_2y").slideToggle("slow");
-	});
-
-	$(".sections").hide();
-	$(".largest").css('visibility', 'hidden');
-	$(".default").show("fold", 1000);
-	$(".largest").css('display', 'block');
-	$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-
-	$(".li_1").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_1").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);		
-	});
-
-	$(".li_2").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_2").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-	});
-
-	$(".li_3").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_3").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-	});
-
-	$(".li_4").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_4").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-	});
-
-	$(".li_5").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_5").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-	});
-
-	$(".li_6").click(function()
-	{
-		$(".sections").hide();
-		$(".largest").css('visibility', 'hidden');
-		$(".temphdr").css("background", "yellow");
-		$("#section_6").show("fold", 1000);
-		$(".largest").css('display', 'block');
-		$(".temphdr").animate({backgroundColor: jQuery.Color({alpha: 0})}, 1500);
-	});
-
-	$(".lrg").click(function()
-	{
-		$(".largest").css('visibility', 'visible');
 	});
 
 	$(".thumb").attr('title', 'Click to see larger version');
@@ -205,6 +111,15 @@ function init ()
 		{
 			$(".boincsection").css('display', 'none');
 			$("#"+$("#boincsections option:selected").val()).css('display', 'block');			
+		}
+	});
+
+	$("#sepsissections").selectmenu(
+	{
+		change: function(event, data)
+		{
+			$(".sepsissection").css('display', 'none');
+			$("#"+$("#sepsissections option:selected").val()).css('display', 'block');			
 		}
 	});
 
